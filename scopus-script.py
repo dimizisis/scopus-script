b07bb08e7219e2581010e1a9e40407a28b27a3da
Dimitrios Zisis <zdimitris@outlook.com>
2019-03-30 20:14:48 +0200

    Added semi-finished scan documents functionality

--- scopus-script.py
+++ scopus-script.py
@@ -24,7 +24,40 @@ def search():
     search_btn = browser.find_element_by_id('advSearch')    # when query is sent, find & press the search button
     search_btn.click()
 
-browser = webdriver.Chrome('C:/Users/Dimitris/Desktop/chromedriver.exe')
+def scan_documents():
+    table_id = WebDriverWait(browser, 10).until(    
+        EC.presence_of_element_located((By.ID, 'srchResultsList'))) # srchResultsList is the data table, from which we will get the documents' names
+    rows = table_id.find_elements(By.TAG_NAME, 'tr') # get all of the rows in the table
+
+    for row in rows:
+        try:        
+            first_col = row.find_elements(By.TAG_NAME, 'td')[0] # get the first column of data table (document name)
+            if 'View abstract' not in first_col.text:
+                document_name = first_col.text # if there is no View Abstract Related Documents text (dirty col), it is a document name
+        except:
+            continue
+        link = row.find_element_by_link_text(document_name)
+        link.click()
+        metrics_span = WebDriverWait(browser, 10).until(    
+        EC.element_to_be_clickable((By.LINK_TEXT, 'View all metrics')))
+        metrics_span.click()
+        percentiles = []
+        percentiles.append(browser.find_element_by_id('percentLabel'))
+        ui_selector = WebDriverWait(browser, 10).until(    
+        EC.element_to_be_clickable((By.ID, 'multipleOptions-button')))
+        ui_selector.click()
+        ui_list = browser.find_element_by_id('multipleOptions-menu')
+        items = ui_list.find_elements_by_tag_name("li")
+        for item in items:
+            try:
+                item.click()
+                percentiles.append(browser.find_element_by_id('percentLabel'))
+            except:
+                continue
+        break    
+
+
+browser = webdriver.Chrome('C:/chromedriver.exe')
 
 url = 'https://www.scopus.com/'
 
@@ -36,3 +69,5 @@ password_str = 'scopus_pass'
 login(username_str, password_str)
 
 search()
+
+scan_documents()
