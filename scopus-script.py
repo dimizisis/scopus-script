139b9e8f5a5fe90dc375a064d189e610f267f0af
Dimitrios Zisis <zdimitris@outlook.com>
2019-04-03 21:00:47 +0300

    Successfully parsing all metrics

--- scopus-script.py
+++ scopus-script.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support. select import Select
+from selenium.common import exceptions
 
 def login(username, password):
     user = browser.find_element_by_id('paywall_username')   # Find the textboxes we'll send the credentials
@@ -28,39 +28,60 @@ def search():
 def scan_documents():
     table_id = WebDriverWait(browser, 10).until(    
         EC.presence_of_element_located((By.ID, 'srchResultsList'))) # srchResultsList is the data table, from which we will get the documents' names
-    rows = table_id.find_elements(By.TAG_NAME, 'tr') # get all of the rows in the table
+    row = table_id.find_element(By.CLASS_NAME, 'ddmDocTitle') # get all of the rows in the table
 
-    for row in rows:
-        try:        
-            first_col = row.find_elements(By.TAG_NAME, 'td')[0] # get the first column of data table (document name)
-            if 'View abstract' not in first_col.text:
-                document_name = first_col.text # if there is no View Abstract Related Documents text (dirty col), it is a document name
+    try:
+        doc = WebDriverWait(browser, 20).until(    
+            EC.presence_of_element_located((By.LINK_TEXT, row.text)))   # go in document's page
+        doc.click()
+    except:
+        exit(1)
+
+    while True:
+
+        try:
+            metrics_span = WebDriverWait(browser, 5).until(    
+            EC.element_to_be_clickable((By.LINK_TEXT, 'View all metrics')))
+            metrics_span.click() # click & go to metrics page (for the specific document)
         except:
-            continue
-        row.find_element_by_link_text(document_name).click() # go in document's page
-        metrics_span = WebDriverWait(browser, 10).until(    
-        EC.element_to_be_clickable((By.LINK_TEXT, 'View all metrics')))
-        metrics_span.click() # click & go to metrics page (for the specific document)
+            next_span = WebDriverWait(browser, 5).until(    
+                EC.element_to_be_clickable((By.LINK_TEXT, 'Next'))) # go to next document
+            if not next_span: exit(0) # if there is no other document, exit
+            next_span.click()
         percentiles = [] # initialize the percentiles list
-        percentiles.append(browser.find_element_by_id('percentLabel')) # get the first percentile
-        print(browser.find_element_by_id('percentLabel').text)
-        menu = WebDriverWait(browser, 10).until(lambda browser: browser.find_element_by_id('multipleOptions-menu'))
-        selector = WebDriverWait(browser, 10).until(lambda browser: browser.find_element_by_id('multipleOptions'))
-        browser.find_element_by_id('multipleOptions-button').click()
-        items = menu.find_elements_by_tag_name('li')
-        i=3
-        for item in items:
-            item_id = 'ui-id-10'+str(i)
-            print(item_id)
+        try:
+            doc_title = browser.find_element_by_id('altmet_article').find_element_by_tag_name('h3').text    # get document title
+            percentiles.append(browser.find_element_by_id('percentLabel').text) # get the first percentile
             try:
-                browser.find_element_by_id('multipleOptions-button').click()
-                menu.find_element_by_id(item_id).click()
-                i+=1
+                menu = browser.find_element_by_id('multipleOptions-menu')   # if dropdown list has multiple options
+                button = browser.find_element_by_id('multipleOptions-button')
+                button.click()
             except:
-                continue
-            percentiles.append(browser.find_element_by_id('percentLabel')) # get the first percentile
-            print(browser.find_element_by_id('percentLabel').text)
-        break    
+                menu = browser.find_element_by_id('subjectArea-menu')   # if dropdown list has only one option
+                button = browser.find_element_by_id('subjectArea-button')
+                
+            items = menu.find_elements_by_tag_name('div')   # find items of dropdown list
+            for item in items:  # get the rest percentiles
+                try:
+                    button.click()  # click button to see options
+                    item.click()    # click on every option of dropdown list
+                    percentiles.append(browser.find_element_by_id('percentLabel').text) # append percentile to list 
+                except:
+                    continue
+            print(doc_title)    # printing
+            for percentile in percentiles:  
+                print(percentile)
+            browser.find_element_by_xpath("//*[text()='Back to document']").click() # got the percentiles, go back
+            next_span = WebDriverWait(browser, 5).until(    
+                EC.element_to_be_clickable((By.LINK_TEXT, 'Next'))) # go to next document
+            if not next_span: exit(0) # if there is no other document, exit
+            next_span.click()
+        except:
+            browser.find_element_by_xpath("//*[text()='Back to document']").click() # if document has no metrics to fetch
+            next_span = WebDriverWait(browser, 5).until(    
+                EC.element_to_be_clickable((By.LINK_TEXT, 'Next'))) # go to next document
+            if not next_span: exit(0) # if there is no other document, exit
+            next_span.click()  
 
 browser = webdriver.Chrome('C:/Users/Dimitris/Desktop/chromedriver.exe')
 
